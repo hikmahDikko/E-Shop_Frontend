7d38efc240cff71a8f2eb699cbd90dde9d5b1e9b
hikmahDikko <dhywis31@gmail.com>
2023-06-04 17:32:09 +0100

    new

--- libs/products/jest.config.ts
+++ libs/products/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'lib',
+  displayName: 'products',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/lib',
+  coverageDirectory: '../../coverage/libs/products',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
--- libs/user/src/lib/user.module.ts
+++ libs/user/src/lib/user.module.ts
@@ -4,4 +4,4 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [CommonModule],
 })
-export class LibModule {}
+export class UserModule {}
